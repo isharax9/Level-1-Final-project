e35caa5c8ff303f05a76d1fdc0cec3185342e2e6
Vidura Vijerathne <viduranox@gmail.com>
2024-05-06 13:57:17 +0530

    add stock model

--- src/models/Stock.java
+++ src/models/Stock.java
@@ -4,10 +4,25 @@
  */
 package models;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author vidur
  */
 public class Stock {
+    private int id;
+    private int productID;
+    private Product product;
+    private int grnID;
+    private double wholeSalePrice;
+    private double retailPrice;
+    private double defaultDiscount;
+    private double availebleQty;
+    private LocalDateTime expDate;
+    private LocalDateTime mnfDate;
+    private String barcode;
+    
+    
     
 }
