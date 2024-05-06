a7aae3610fd251eb7ecd75c0e21ed39c3d209d7a
Vidura Vijerathne <viduranox@gmail.com>
2024-05-06 14:07:24 +0530

    add invoice item model

--- src/models/InvoiceItem.java
+++ src/models/InvoiceItem.java
@@ -4,8 +4,6 @@
  */
 package models;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,17 +15,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor //generating a No args constructure , LOMBOC ANNOTATION LIB
 @Data // setters and getters
-public class Invoice {
-    private int id;
-    private int employeeID;
-    private Employee employee;
-    private Customer customer;
-    private double total;
-    private double totalDiscount;
-    private double grandTotal;
-    private double paidAmount;
-    private double balance;
-    private LocalDateTime invoiceDate;
-    private List<InvoiceItem> items;
+public class InvoiceItem {
+    private int invoiceItemID;
+    private int stockID;
+    private Stock stock;
+    private double qty;
+    private double dicount;
+    private double value;
+    private double unitPrice;
     
 }
