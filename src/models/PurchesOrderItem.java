46575aa76057c3c74a62a02f2c38172e237e1374
Vidura Vijerathne <viduranox@gmail.com>
2024-05-06 13:42:00 +0530

    add Purches order model

---
+++ src/models/PurchesOrderItem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author vidur
+ */
+public class PurchesOrderItem {
+    private int productID;
+    private int poID;
+    private double qty;
+    private Product Product;
+}
