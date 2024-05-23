afb3d6ca82be1ffee8109a9b184ff1588092619b
Vidura Vijerathne <viduranox@gmail.com>
2024-05-23 10:09:15 +0530

    making a encasulated Auth object

---
+++ src/models/Auth.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import lombok.AllArgsConstructor;
+
+/**
+ *
+ * @author vidur
+ */
+@AllArgsConstructor
+public class Auth {
+    private int userID;
+    private String username;
+    private String password;
+    private Employee employee;
+    private UserType authType;
+    
+    
+    private String getUsername(){
+        return this.username;
+    }
+    private String getPassword(){
+        return this.password;
+    }
+    private Employee getEmployee(){
+        return this.employee;
+    }
+    private int getUserID(){
+        return this.userID;
+    }
+    private UserType getAuthType(){
+        return this.authType;
+    }
+    
+    private boolean isAuthenticated(){
+        return false;
+    }
+    
+    
+}
