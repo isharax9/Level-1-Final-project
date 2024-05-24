195546f86c7cf81c805b76d834768136ee786a74
Vidura Vijerathne <viduranox@gmail.com>
2024-05-24 19:45:07 +0530

    fix:connection issue

--- src/dao/CategoryDAO.java
+++ src/dao/CategoryDAO.java
@@ -13,44 +13,65 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  *
  * @author vidur
  */
 public class CategoryDAO {
-    Database database ; //singolton Instance of 
 
-    public CategoryDAO() {
-        this.database = Database.getInstance();
-    }
-    
+   
     public void createCategory(Category category) throws SQLException {
+         Database database = Database.getInstance();
         String query = "INSERT INTO categories (category) VALUES (?)";
         Connection conn = database.getConnection();
-         PreparedStatement statement = conn.prepareStatement(query);
-         statement.setString(1,category.getCategory());
-         ResultSet results = statement.executeQuery();
+        PreparedStatement statement = conn.prepareStatement(query);
+        statement.setString(1, category.getCategory());
+        statement.executeUpdate();
     }
-    
+
     public List<Category> readCategories() throws SQLException {
+         Database database = Database.getInstance();
         List<Category> categories = new ArrayList<>();
         String query = "SELECT * FROM categories";
         Connection conn = database.getConnection();
         PreparedStatement statement = conn.prepareStatement(query);
         ResultSet results = statement.executeQuery();
-        while(results.next()){
+        while (results.next()) {
             categories.add(Category.fromResultSet(results));
         }
-        return categories;        
+        return categories;
     }
-    
-    
-     public void updateCategory(Category category) throws SQLException {
+
+    public void updateCategory(Category category) throws SQLException {
+         Database database = Database.getInstance();
         String query = "UPDATE categories SET category = ? WHERE cat_id = ?";
-         Connection conn = database.getConnection();
+        Connection conn = database.getConnection();
         PreparedStatement statement = conn.prepareStatement(query);
         statement.setString(1, category.getCategory());
-        statement.setInt(2,category.getId());
+        statement.setInt(2, category.getId());
         ResultSet results = statement.executeQuery();
-     }
+    }
+
+    public Category getCategoryByName(String name) throws SQLException, IllegalArgumentException {
+        Database database = Database.getInstance();
+       
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Category name cannot be empty");
+        }
+
+        String query = "SELECT * FROM categories WHERE category = ?";
+
+        try (Connection conn = database.getConnection(); PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.setString(1, name);
+
+            try (ResultSet result = statement.executeQuery()) {
+                if (result.next()) {
+                    return Category.fromResultSet(result);
+                } else {
+                    throw new IllegalArgumentException("Category not found");
+                }
+            }
+        }
+    }
 }
--- src/services/CategoryService.java
+++ src/services/CategoryService.java
@@ -15,42 +15,77 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author vidur
  */
 public class CategoryService {
-    
-     private CategoryDAO categoryDAO;
+
+    private CategoryDAO categoryDAO;
 
     public CategoryService() {
-        categoryDAO =new CategoryDAO();
+        categoryDAO = new CategoryDAO();
     }
-    
-    public void createCategory(Category category) throws SQLException,IllegalArgumentException {
-        try{
-            if(category.isValidate()){
-                categoryDAO.createCategory(category);
+
+    public void createCategory(Category category) throws SQLException, IllegalArgumentException {
+        try {
+            if (category.isValidate()) {
+                
+//                check already Exist Cat name
+                boolean isCategoryAlreadyExist = false;
+                try{
+                    var exitCat = categoryDAO.getCategoryByName(category.getCategory());
+                    if(exitCat !=null){
+                        isCategoryAlreadyExist = true;
+                    }
+                    
+                }catch(Exception ex){
+                    
+                }
+                if(!isCategoryAlreadyExist){
+                    categoryDAO.createCategory(category);
+                }else{
+                    throw new IllegalArgumentException("Category Name is already Exist!");
+                }
+                
             }
-        }catch(IllegalArgumentException ex){
+        } catch (IllegalArgumentException ex) {
             throw ex;
         }
-        
+
     }
 
     public List<Category> getAllCategories() throws SQLException {
         return categoryDAO.readCategories();
     }
 
-    public void updateCategory(Category category) throws SQLException,IllegalArgumentException  {
-        if(category.getId() == 0){
+    public void updateCategory(Category category) throws SQLException, IllegalArgumentException {
+        if (category.getId() == 0) {
             throw new IllegalArgumentException("Invalid category id");
         }
-        if(category.isValidate()){
+        if (category.isValidate()) {
             categoryDAO.updateCategory(category);
         }
-        
+
     }
-    
-     
+
+    public static void main(String[] args) {
+
+        Category cat = new Category(0, "CatAAAD");
+        CategoryService categoryService = new CategoryService();
+        try {
+            categoryService.createCategory(cat);
+            System.out.println("DONE");
+        }catch(IllegalArgumentException ex){
+            
+            JOptionPane.showMessageDialog(null,ex,"UserInputError",JOptionPane.ERROR_MESSAGE);
+        }catch(SQLException ex){
+            System.out.println(ex);
+            JOptionPane.showMessageDialog(null,ex,"DB Error",JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+
 }
