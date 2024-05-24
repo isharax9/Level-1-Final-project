256d9dc8249e8ea05a06bbebaa3b90934c46d459
Vidura Vijerathne <viduranox@gmail.com>
2024-05-24 22:24:34 +0530

    add unit DAO,Serivice and update unit Validations

---
+++ src/dao/UnitDAO.java
@@ -0,0 +1,84 @@
+package dao;
+
+import models.Unit;
+import utils.Database;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnitDAO {
+
+    public void createUnit(Unit unit) throws SQLException {
+        String query = "INSERT INTO units (unit) VALUES (?)";
+        Connection conn = Database.getInstance().getConnection();
+        PreparedStatement statement = conn.prepareStatement(query);
+        statement.setString(1, unit.getUnit().toLowerCase());
+        statement.executeUpdate();
+    }
+
+    public List<Unit> readUnits() throws SQLException {
+        List<Unit> units = new ArrayList<>();
+        String query = "SELECT * FROM units";
+        Connection conn = Database.getInstance().getConnection();
+        PreparedStatement statement = conn.prepareStatement(query);
+        ResultSet results = statement.executeQuery();
+        while (results.next()) {
+            units.add(Unit.fromResultSet(results));
+
+        }
+        return units;
+    }
+
+    public void updateUnit(Unit unit) throws SQLException {
+        String query = "UPDATE units SET unit = ? WHERE unit_id = ?";
+        Connection conn = Database.getInstance().getConnection();
+        PreparedStatement statement = conn.prepareStatement(query);
+        statement.setString(1, unit.getUnit().toLowerCase());
+        statement.setInt(2, unit.getId());
+        statement.executeUpdate();
+    }
+
+    public Unit getUnitByID(int id) throws SQLException {
+        String query = "SELECT * FROM units WHERE unit_id = ?";
+        Connection conn = Database.getInstance().getConnection();
+        PreparedStatement statement = conn.prepareStatement(query);
+        statement.setInt(1, id);
+        try (ResultSet result = statement.executeQuery()) {
+            if (result.next()) {
+                return Unit.fromResultSet(result);
+            } else {
+                throw new IllegalArgumentException("Unit not found");
+            }
+        }
+    }
+
+    public Unit getUnitByName(String name) throws SQLException {
+        String query = "SELECT * FROM units WHERE unit = ?";
+                Connection conn = Database.getInstance().getConnection();
+                PreparedStatement statement = conn.prepareStatement(query);
+            statement.setString(1, name.toLowerCase());
+            try (ResultSet result = statement.executeQuery()) {
+                if (result.next()) {
+                    return Unit.fromResultSet(result);
+                } else {
+                    throw new IllegalArgumentException("Unit not found");
+                }
+            
+        }
+    }
+
+    public List<Unit> searchUnitsByName(String name) throws SQLException {
+        List<Unit> units = new ArrayList<>();
+        String query = "SELECT * FROM units WHERE unit LIKE ?";
+        Connection conn = Database.getInstance().getConnection();
+        PreparedStatement statement = conn.prepareStatement(query);
+            statement.setString(1, name.toLowerCase() + "%");
+            try (ResultSet results = statement.executeQuery()) {
+                while (results.next()) {
+                    units.add(Unit.fromResultSet(results));
+                }
+        }
+        return units;
+    }
+}
---
+++ src/services/UnitService.java
@@ -0,0 +1,130 @@
+package services;
+
+import dao.UnitDAO;
+import models.Unit;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UnitService {
+
+    private final UnitDAO unitDAO;
+
+    public UnitService() {
+        unitDAO = new UnitDAO();
+    }
+
+    public void createUnit(Unit unit) throws SQLException, IllegalArgumentException {
+        try {
+            if (unit.isValidate()) {
+                boolean isUnitAlreadyExist = false;
+                try {
+                    Unit existingUnit = unitDAO.getUnitByName(unit.getUnit());
+                    if (existingUnit != null) {
+                        isUnitAlreadyExist = true;
+                    }
+                } catch (Exception ex) {
+                    // Log the exception if needed
+                }
+                if (!isUnitAlreadyExist) {
+                    unitDAO.createUnit(unit);
+                } else {
+                    throw new IllegalArgumentException("Unit already exists!");
+                }
+            }
+        } catch (IllegalArgumentException ex) {
+            throw ex;
+        }
+    }
+
+    public List<Unit> getAllUnits() throws SQLException {
+        return unitDAO.readUnits();
+    }
+
+    public void updateUnit(Unit unit) throws SQLException, IllegalArgumentException {
+        if (unit.getId() <= 0) {
+            throw new IllegalArgumentException("Invalid unit id");
+        }
+        if (unit.isValidate()) {
+            Unit existUnit = getUnitByID(unit.getId());
+            if (existUnit != null) {
+                unitDAO.updateUnit(unit);
+            } else {
+                throw new IllegalArgumentException("unit not found to update");
+            }
+
+        }
+    }
+
+    public Unit getUnitByID(int id) throws SQLException, IllegalArgumentException {
+        if (id == 0) {
+            throw new IllegalArgumentException("Unit id cannot be 0");
+        }
+        return unitDAO.getUnitByID(id);
+    }
+
+    public Unit getUnitByName(String name) throws SQLException, IllegalArgumentException {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Unit name cannot be empty");
+        }
+        return unitDAO.getUnitByName(name);
+    }
+
+    public List<Unit> searchUnitsByName(String name) throws SQLException, IllegalArgumentException {
+        if (name == null || name.isEmpty()) {
+            return List.of();
+        }
+        return unitDAO.searchUnitsByName(name);
+    }
+
+    public static void main(String[] args) {
+        UnitService unitService = new UnitService();
+
+        // Example for adding a new Unit
+//        Unit unitToAdd = new Unit();
+//        unitToAdd.setUnit("New Unit");
+//        try {
+//            unitService.createUnit(unitToAdd);
+//            System.out.println("Unit added successfully!");
+//        } catch (SQLException | IllegalArgumentException e) {
+//            e.printStackTrace();
+//        }
+
+        // Example for getting all Units
+//        try {
+//            List<Unit> allUnits = unitService.getAllUnits();
+//            System.out.println("All Units: " + allUnits);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+// Example for updating a Unit
+//        Unit unitToUpdate = new Unit();
+//        unitToUpdate.setId(2); // Assuming this is the id of the Unit you want to update
+//        unitToUpdate.setUnit("");
+//        try {
+//            unitService.updateUnit(unitToUpdate);
+//            System.out.println("Unit updated successfully!");
+//        } catch (SQLException | IllegalArgumentException e) {
+//            e.printStackTrace();
+//        }
+
+        // Example for getting a Unit by its id
+//        try {
+//            Unit foundUnit = unitService.getUnitByID(100); // Assuming this is the id of the Unit you want to find
+//            System.out.println("Found Unit: " + foundUnit);
+//        } catch (SQLException | IllegalArgumentException e) {
+//            e.printStackTrace();
+//        }
+
+        // Example for searching Units by name
+//        String unitName = "Up";
+//        try {
+//            List<Unit> searchResults = unitService.searchUnitsByName(unitName);
+//            System.out.println("Search results for '" + unitName + "': " + searchResults);
+//        } catch (SQLException | IllegalArgumentException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
