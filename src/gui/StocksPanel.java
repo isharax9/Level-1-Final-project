fef4bd2b2b567456306ee17d21c6aec5c8cddb6a
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-06-02 20:48:58 +0530

    added expStocks & GRN reports

--- src/gui/StocksPanel.java
+++ src/gui/StocksPanel.java
@@ -13,6 +13,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+import services.PrintService;
 import services.StockService;
 import services.SupplierService;
 
@@ -99,6 +100,7 @@ public class StocksPanel extends javax.swing.JPanel {
         btn_search_Stocks = new javax.swing.JButton();
         btn_deactivate_Stocks = new javax.swing.JButton();
         btn_view_grn_Stocks = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tbt_stock = new javax.swing.JTable();
@@ -143,6 +145,13 @@ public class StocksPanel extends javax.swing.JPanel {
 
         btn_view_grn_Stocks.setText("View Grn");
 
+        jButton1.setText("Print Exp Stocks Reports");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -157,21 +166,24 @@ public class StocksPanel extends javax.swing.JPanel {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(btn_view_grn_Stocks))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(8, 8, 8)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel5)
-                            .addComponent(jLabel6))
-                        .addGap(18, 18, 18)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(tf_barcode_Stocks)
-                            .addComponent(tf_product_name_Stocks)
-                            .addComponent(tf_suppler_Stocks)
-                            .addComponent(jCheckBox1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(dc_mnf_date_Stocks, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(dc_exp_date_Stocks, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(8, 8, 8)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jLabel3)
+                                    .addComponent(jLabel4)
+                                    .addComponent(jLabel5)
+                                    .addComponent(jLabel6))
+                                .addGap(18, 18, 18)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(tf_barcode_Stocks)
+                                    .addComponent(tf_product_name_Stocks)
+                                    .addComponent(tf_suppler_Stocks)
+                                    .addComponent(jCheckBox1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(dc_mnf_date_Stocks, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(dc_exp_date_Stocks, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(jButton1))
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -205,6 +217,8 @@ public class StocksPanel extends javax.swing.JPanel {
                     .addComponent(btn_search_Stocks)
                     .addComponent(btn_deactivate_Stocks)
                     .addComponent(btn_view_grn_Stocks))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -309,6 +323,11 @@ public class StocksPanel extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_tbt_stockMouseClicked
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        new PrintService().PrintExpriedStocks(stockList);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn_deactivate_Stocks;
@@ -316,6 +335,7 @@ public class StocksPanel extends javax.swing.JPanel {
     private javax.swing.JButton btn_view_grn_Stocks;
     private com.toedter.calendar.JDateChooser dc_exp_date_Stocks;
     private com.toedter.calendar.JDateChooser dc_mnf_date_Stocks;
+    private javax.swing.JButton jButton1;
     private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
--- src/services/PrintService.java
+++ src/services/PrintService.java
@@ -4,9 +4,12 @@
  */
 package services;
 
+import dto.GRN;
 import dto.Invoice;
 import dto.InvoiceItem;
+import dto.Stock;
 import dto.UserType;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -25,11 +28,17 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
  * @author vidur
  */
 public class PrintService {
-    
+
     public static String getCurrentTimeFormatted() {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/M/dd hh:mm a");
         return now.format(formatter);
+    }
+
+    public static String dateFormatter(Timestamp now) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/M/dd hh:mm a");
+        return now.toLocalDateTime().format(formatter);
     }
 
     public static void PrintInvoice(Invoice invoice) {
@@ -39,50 +48,124 @@ public class PrintService {
             List<Map<String, Object>> dataSource = new ArrayList<>();
             double totalForFooter = 0;
             double totalDiscount = 0;
-            for(InvoiceItem invoiceItem : invoice.getItems()){
-                 Map<String, Object> item = new HashMap<>();
-                    item.put("product_name",invoiceItem.getStock().getGrn().getPurchaseOrder().getProduct().getProductName());
-                    item.put("qty", "X" + invoiceItem.getInvoiceItemQty());
-                    item.put("unit_price", "රු. " + String.format("%.2f", invoiceItem.getStock().getUnitPrice()));
-                    item.put("discount", "රු. " + String.format("%.2f", invoiceItem.getInvoiceItemDiscount()));
-                    double subtotal = invoiceItem.getInvoiceItemQty()* (invoiceItem.getStock().getUnitPrice()- invoiceItem.getInvoiceItemDiscount());
-                    item.put("cost", "රු. " + String.format("%.2f", subtotal));
-                    dataSource.add(item);
-                    totalForFooter += invoiceItem.getInvoiceItemQty()* invoiceItem.getStock().getUnitPrice();
-                    totalDiscount += invoiceItem.getInvoiceItemDiscount()*invoiceItem.getInvoiceItemQty();
+            for (InvoiceItem invoiceItem : invoice.getItems()) {
+                Map<String, Object> item = new HashMap<>();
+                item.put("product_name", invoiceItem.getStock().getGrn().getPurchaseOrder().getProduct().getProductName());
+                item.put("qty", "X" + invoiceItem.getInvoiceItemQty());
+                item.put("unit_price", "රු. " + String.format("%.2f", invoiceItem.getStock().getUnitPrice()));
+                item.put("discount", "රු. " + String.format("%.2f", invoiceItem.getInvoiceItemDiscount()));
+                double subtotal = invoiceItem.getInvoiceItemQty() * (invoiceItem.getStock().getUnitPrice() - invoiceItem.getInvoiceItemDiscount());
+                item.put("cost", "රු. " + String.format("%.2f", subtotal));
+                dataSource.add(item);
+                totalForFooter += invoiceItem.getInvoiceItemQty() * invoiceItem.getStock().getUnitPrice();
+                totalDiscount += invoiceItem.getInvoiceItemDiscount() * invoiceItem.getInvoiceItemQty();
             }
-            
-            Map<String, Object> parameters = new HashMap<>();
-                parameters.put("invoice_no", invoice.getId()+"");
-                parameters.put("invoice_date",getCurrentTimeFormatted());
-                parameters.put("total_cost_without_discount", "Total: LKR."+ String.format("%.2f", totalForFooter));
-                parameters.put("total_discount", "Total Discount : LKR."+String.format("%.2f", totalDiscount));
-                parameters.put("invoice_total", "Grand Total : LKR."+String.format("%.2f",totalForFooter-totalDiscount ));
-                parameters.put("total_discount_greeting", "");
-                
-                double cash = invoice.getPaidAmount();
-                if (cash > 0) {
-                    parameters.put("cash", "Cash : LKR."+String.format("%.2f",invoice.getPaidAmount()) );
-                    parameters.put("balance", "Balance : LKR."+String.format("%.2f",invoice.getPaidAmount()-(totalForFooter-totalDiscount)));
-                    parameters.put("line1", "___________");
-                } else {
-                    parameters.put("cash", "CARDPAYMENT");
-                    parameters.put("balance", "");
-                    parameters.put("line1", "___________");
-                }
-                parameters.put("CashierName",invoice.getEmployee().getFirstName()+" "+invoice.getEmployee().getLastName());
-                
-                JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JRBeanCollectionDataSource(dataSource));
 
-                JasperPrintManager.printReport(jasperPrint, false);
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("invoice_no", invoice.getId() + "");
+            parameters.put("invoice_date", getCurrentTimeFormatted());
+            parameters.put("total_cost_without_discount", "Total: LKR." + String.format("%.2f", totalForFooter));
+            parameters.put("total_discount", "Total Discount : LKR." + String.format("%.2f", totalDiscount));
+            parameters.put("invoice_total", "Grand Total : LKR." + String.format("%.2f", totalForFooter - totalDiscount));
+            parameters.put("total_discount_greeting", "");
+
+            double cash = invoice.getPaidAmount();
+            if (cash > 0) {
+                parameters.put("cash", "Cash : LKR." + String.format("%.2f", invoice.getPaidAmount()));
+                parameters.put("balance", "Balance : LKR." + String.format("%.2f", invoice.getPaidAmount() - (totalForFooter - totalDiscount)));
+                parameters.put("line1", "___________");
+            } else {
+                parameters.put("cash", "CARDPAYMENT");
+                parameters.put("balance", "");
+                parameters.put("line1", "___________");
+            }
+            parameters.put("CashierName", invoice.getEmployee().getFirstName() + " " + invoice.getEmployee().getLastName());
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JRBeanCollectionDataSource(dataSource));
+
+            JasperPrintManager.printReport(jasperPrint, false);
 
         } catch (Exception ex) {
             System.out.println(ex);
             ex.printStackTrace();
-            
+
         }
-        
-        
+
+    }
+
+    public void PrintGrn(GRN grn) {
+        System.out.println("GRN");
+        try {
+            JasperReport jasperReport = JasperCompileManager.compileReport("src/reports/grn.jrxml");
+
+            List<Map<String, Object>> dataSource = new ArrayList<>();
+
+            Map<String, Object> item = new HashMap<>();
+            item.put("itemField1", "value1");
+            item.put("itemField2", "value2");
+            // Add other fields as per the report's requirements.
+            dataSource.add(item);
+
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("grnId", grn.getId() + "");
+            parameters.put("grnDate", dateFormatter(grn.getGrnDate()));
+            parameters.put("purchaseOrderId", grn.getPurchaseOrder().getId() + "");
+            parameters.put("purchaseOrderDate", grn.getPurchaseOrder().getOrderedDate().toString());
+            parameters.put("prName", grn.getPurchaseOrder().getProduct().getProductName());
+            parameters.put("wholeSalePrice", String.format("%.2f", grn.getPurchaseOrder().getWholesaleUnitPrice()));
+            parameters.put("paidAmount", String.format("%.2f", grn.getPurchaseOrder().getPaidAmount()));
+            parameters.put("supplierName", grn.getPurchaseOrder().getSupplier().getFirstName() + " " + grn.getPurchaseOrder().getSupplier().getLastName());
+            parameters.put("supContact", grn.getPurchaseOrder().getSupplier().getContact());
+            parameters.put("printedDate", getCurrentTimeFormatted());
+            parameters.put("orderQty", String.format("%.2f", grn.getPurchaseOrder().getOrderQty()));
+
+//            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JRBeanCollectionDataSource(dataSource));
+//
+//            JasperPrintManager.printReport(jasperPrint, false);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JRBeanCollectionDataSource(dataSource));
+
+            JasperPrintManager.printReport(jasperPrint, false);
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+            ex.printStackTrace();
+
+        }
+    }
+
+    public void PrintExpriedStocks(List<Stock> stocks) {
+        System.out.println("EXP STOCK REPORT ");
+        try {
+            JasperReport jasperReport = JasperCompileManager.compileReport("src/reports/expStocks.jrxml");
+            List<Map<String, Object>> dataSource = new ArrayList<>();
+
+            for (Stock stock : stocks) {
+                Map<String, Object> item = new HashMap<>();
+                item.put("barcode", stock.getStockBarcode()+"");
+                item.put("grnID", stock.getGrn().getId()+"");
+                item.put("mnfDate",  stock.getMnfDate().toString());
+                item.put("expDate", stock.getExpDate().toString());
+                item.put("unitPrice", String.format("%.2f", stock.getUnitPrice()));
+                item.put("expQty", "" +stock.getAvailableQty() );
+                item.put("discount", "රු. " + String.format("%.2f", stock.getDiscount()));
+                item.put("productName", "" + stock.getGrn().getPurchaseOrder().getProduct().getProductName());
+
+                dataSource.add(item);
+                
+            }
+
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("printedDate", getCurrentTimeFormatted());
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JRBeanCollectionDataSource(dataSource));
+
+            JasperPrintManager.printReport(jasperPrint, false);
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+            ex.printStackTrace();
+
+        }
     }
 
 }
